ed7ec711dc238f94c54befb5b45098ec68c30471
Samantha Asefi <samanthamilan@gmail.com>
2025-03-23 17:46:35 -0400

    adding doc

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='booklover',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/smasefi/hw09',
+    author='Samantha Asefi',
+    author_email='sma9am@gmail.com',
+    license='MIT',
+    packages=['booklover']
+)
